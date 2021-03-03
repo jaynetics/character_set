657a2d075ab0149a40d125d9a2470ad69a2d8868
Jannosch Müller <janosch.mueller@betterplace.org>
2021-03-03 16:23:33 +0100

    Add spec

--- spec/character_set_spec.rb
+++ spec/character_set_spec.rb
@@ -2,4 +2,12 @@ describe CharacterSet do
   it 'has a version number' do
     expect(CharacterSet::VERSION).not_to be nil
   end
+
+  it 'uses the RubyFallback in non-C rubies' do
+    if RUBY_PLATFORM[/java/i]
+      expect(CharacterSet.ancestors).to include(CharacterSet::RubyFallback)
+    else
+      expect(CharacterSet.ancestors).not_to include(CharacterSet::RubyFallback)
+    end
+  end
 end
