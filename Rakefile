cee03e973bf54b0500b771ffef650eadec623f20
Janosch Müller <janosch.mueller@betterplace.org>
2022-04-11 22:01:22 +0200

    WIP

--- Rakefile
+++ Rakefile
@@ -41,3 +41,22 @@ unless RUBY_PLATFORM =~ /java/
   task(:benchmark).enhance([:compile])
   task(:spec).enhance([:compile])
 end
+
+# WIP - build encoding cp lists
+require 'character_set'
+require 'range_compressor'
+
+enc_names = Encoding.list.map(&:name).sort
+hash = {}
+full_s = CharacterSet.valid.map { |cp| cp.chr('utf-8') }.join; 1
+enc_names.each do |name|
+  enc_s = full_s.encode(name, invalid: :replace, undef: :replace, replace: '')
+  enc_cs = CharacterSet.of(enc_s)
+  cp_str = enc_cs.ranges.map { |r| "#{r.min.to_s(16)},#{r.max.to_s(16)}\n" }.join.upcase
+  hash[name] = cp_str
+rescue => e
+  hash[name] = e
+end
+
+good = hash.select { |k, v| !v.is_a?(Exception) }; 1
+shared = good.keys.group_by { |k| good[k] }.map { |k, v| [v, k] }.to_h
--- lib/character_set/predefined_sets.rb
+++ lib/character_set/predefined_sets.rb
@@ -1,6 +1,8 @@
 class CharacterSet
   module PredefinedSets
-    Dir[File.join(__dir__, 'predefined_sets', '*.cps')].each do |path|
+    sets = Dir[File.join(__dir__, 'predefined_sets', '*.cps')]
+
+    sets.each do |path|
       set_name = File.basename(path, '.cps')
 
       class_eval <<-RUBY, __FILE__, __LINE__ + 1
@@ -13,13 +15,6 @@ class CharacterSet
         end
       RUBY
     end
-
-    alias all                      any
-    alias ascii_letters            ascii_letter
-    alias basic_multilingual_plane bmp
-    alias blank                    whitespace
-    alias invalid                  surrogate
-    alias valid                    unicode
 
     def build_from_cps_file(path)
       if defined?(Ractor) && Ractor.current != Ractor.main
@@ -38,5 +33,33 @@ class CharacterSet
         set.merge((range_start.to_i(16))..(range_end.to_i(16)))
       end
     end
+
+    aliases = {
+      all:                      :any,
+      ascii_letters:            :ascii_letter,
+      basic_multilingual_plane: :bmp,
+      blank:                    :whitespace,
+      invalid:                  :surrogate,
+      us_ascii:                 :ascii,
+      utf_8:                    :unicode,
+      utf_16:                   :unicode,
+      utf_16be:                 :unicode,
+      utf_16le:                 :unicode,
+      utf_32:                   :unicode,
+      valid:                    :unicode,
+    }
+
+    aliases.each { |from, to| alias_method(from, to) }
+
+    names = sets.flat_map do |path|
+      set_name = File.basename(path, '.cps')
+      [set_name.to_sym, "non_#{set_name}".to_sym]
+    end + aliases.keys
+
+    class_eval <<-RUBY, __FILE__, __LINE__ + 1
+      def predefined_names
+        #{names}
+      end
+    RUBY
   end
 end
