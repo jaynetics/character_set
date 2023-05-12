983f972a67604699fc91b9b2812acf6027641bb6
Janosch Müller <janosch84@gmail.com>
2023-05-12 17:51:49 +0200

    Lint

--- spec/character_set/spaceship_operator_spec.rb
+++ spec/character_set/spaceship_operator_spec.rb
@@ -1,8 +1,8 @@
 shared_examples :character_set_spaceship_operator do |variant|
   it 'compares left and right' do
-    expect(variant[97] <=> variant[97]).to eq 0
-    expect(variant[97, 98] <=> variant[97]).to eq 1
-    expect(variant[97] <=> variant[97, 98]).to eq -1
+    expect(variant[97] <=> variant[97]).to eq(0) # rubocop:disable Lint
+    expect(variant[97, 98] <=> variant[97]).to eq(1)
+    expect(variant[97] <=> variant[97, 98]).to eq(-1)
   end
 end
 
